e844f93bc544bedd1917ea1ae044647363dc63ee
BryanRosillo <108194629+BryanRosillo@users.noreply.github.com>
2024-08-04 03:24:51 -0500

    fix: lógica en el servicio de seguimiento de malla

--- backend/syncademic/views/seguimiento_api_view.py
+++ backend/syncademic/views/seguimiento_api_view.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from syncademic.services.seguimiento_malla_service import SeguimientoService
-from ..serializers.estudiante_serializer import EstudianteSerializer
+from ..serializers.estudiante_nota_promedio import EstudianteNotaPromedioSerializer
 
 
 class SeguimientoMallaAPIView(APIView):
     def get(self, request, asignatura_prerequisito, periodo_actual):
         seguimiento_service = SeguimientoService(asignatura_prerequisito, periodo_actual)
         estudiantes = seguimiento_service.obtener_estudiantes_candidatos()
-        serializer = EstudianteSerializer(estudiantes, many=True)
+        serializer = EstudianteNotaPromedioSerializer(estudiantes, many=True)
         return Response(serializer.data)
