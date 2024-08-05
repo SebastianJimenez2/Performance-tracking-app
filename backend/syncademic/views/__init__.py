00011a58f59c809ff687c263dab688e12fa4b087
david <david.yanez01@epn.edu.ec>
2024-08-04 23:27:46 -0500

    Merge branch 'main' of https://github.com/JoelDelgado246/Performance-tracking-app

--- backend/syncademic/views/__init__.py
+++ backend/syncademic/views/__init__.py
@@ -1,5 +1,8 @@
 from syncademic.views.notas_api_view import ControlNotasAPIView
+<<<<<<< Updated upstream
 from syncademic.views.asistencia_api_view import AsistenciaAPIView
+=======
 from syncademic.views.asistencia_api_view import AsistenciaAPIView
 from syncademic.views.cronograma_api_view import CronogramaAPIView
 from syncademic.views.tema_cronograma_api_view import TemaCronogramaAPIView
+>>>>>>> Stashed changes
