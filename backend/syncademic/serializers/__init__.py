9611dfc1e5bd681c045f2e5d04529c8e06adabef
david <david.yanez01@epn.edu.ec>
2024-08-04 23:32:27 -0500

    fix: Serializers, services y views arreglados

--- backend/syncademic/serializers/__init__.py
+++ backend/syncademic/serializers/__init__.py
@@ -2,8 +2,7 @@ from .notas_serializer import NotasSerializer
 from .estudiante_serializer import EstudianteSerializer, ListaEstudianteSerializer
 from .asistencia_serializer import AsistenciaSerializer
 from .asignatura_serializer import AsignaturaSerializer
-<<<<<<< Updated upstream
-=======
+
 from .cronograma_serializer import CronogramaSerializer
 from .tema_cronograma_serializer import TemaCronogramaSerializer
->>>>>>> Stashed changes
+
