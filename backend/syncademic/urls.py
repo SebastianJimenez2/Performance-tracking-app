0a885df0d851438122d9812765d2e0a7328b113e
Xavier <94008723+xaviercarpio13@users.noreply.github.com>
2024-08-05 20:39:53 -0500

    feat: agregación de enrutamiento para F7

--- backend/syncademic/urls.py
+++ backend/syncademic/urls.py
@@ -6,9 +6,13 @@ from rest_framework.routers import DefaultRouter
 from syncademic.views import (
     ControlNotasAPIView
 )
+from syncademic.views import (
+evaluacion_docente_api_view
+)
 
 router = DefaultRouter()
 router.register(r'control-notas', ControlNotasAPIView, basename='control-notas')
+router.register(r'evaluacion-docente', evaluacion_docente_api_view, basename='evaluacion-docente')
 
 urlpatterns = [
     path("seguimiento/<str:asignatura_prerequisito>/<str:periodo_actual>/", SeguimientoMallaAPIView.as_view(), name='seguimiento-malla')
