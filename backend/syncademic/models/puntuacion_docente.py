0ef9a17e732e25aceb5fa4eca82a60f222321710
Chris <chris.athan12@gmail.com>
2024-08-07 08:57:13 -0500

    fix: documentacion en cada archivo referente a la feature 8

--- backend/syncademic/models/puntuacion_docente.py
+++ backend/syncademic/models/puntuacion_docente.py
@@ -5,10 +5,21 @@ from syncademic.models.periodo import Periodo
 
 
 class Puntuacion_docente(models.Model):
+    """ Modelo Puntuacion_docente
+
+                Utilizado para Feature 8
+                Creado por Christopher Zambrano
+
+                Attributes:
+                    id_puntuacion (int),
+                    id_docente (int),
+                    periodo (int),
+                    puntaje (char)
+            """
     id_puntuacion = models.IntegerField(primary_key=True)
     id_docente = models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True)
     periodo = models.ForeignKey(Periodo, on_delete=models.SET_DEFAULT, default=1)
-    puntaje = models.CharField(max_length=50)
+    puntaje = models.IntegerField(max_length=50)
 
     def __str__(self):
         return self.docente + self.puntaje
