ea6a92c9395b003475d1e27c09881402a4f61cec
Sebastian <104236657+SebastianJimenez2@users.noreply.github.com>
2024-08-07 09:07:15 -0500

    fix: función para quitar tildes y espacios para que funcione mi API

--- frontend/src/services/EstudiantesCandidatos.ts
+++ frontend/src/services/EstudiantesCandidatos.ts
@@ -1,7 +1,7 @@
 import { EstudianteCandidato } from "../types/EstudiantesCandidatos";
 
 export const obtenerSeguimientoMalla = async (tituloCurso: string): Promise<EstudianteCandidato[] | { error: string }> => {
-    const url = `https://syncademic-0-1.onrender.com/syncademic/seguimiento-malla/${tituloCurso}/2024B/`;
+    const url = `https://syncademic-0-1.onrender.com/syncademic/seguimiento-malla/${quitarEspaciosYAcentos(tituloCurso)}/2024B/`;
 
     try {
         const response = await fetch(url, {
@@ -26,3 +26,18 @@ export const obtenerSeguimientoMalla = async (tituloCurso: string): Promise<Estu
         throw error;
     }
 };
+
+function quitarEspaciosYAcentos(text: string): string {
+        // Mapeo de caracteres con tilde a caracteres sin tilde
+        const accentsMap: { [key: string]: string } = {
+            'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+            'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U'
+        };
+    
+        // Eliminar espacios y reemplazar caracteres con tilde
+        return text
+            .split('') // Convertir la cadena en un array de caracteres
+            .map(char => accentsMap[char] || char) // Reemplazar caracteres con tilde
+            .join('') // Unir los caracteres de nuevo en una cadena
+            .replace(/\s+/g, ''); // Eliminar espacios
+    }
