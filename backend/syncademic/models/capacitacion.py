a7eec7c7e3afe6bdc601b4b5aa7dcb79142e94b8
dacol <da.colcha@hotmail.com>
2024-08-06 21:45:33 -0500

    fix: atributo NULL modelo Capacitacion

--- backend/syncademic/models/capacitacion.py
+++ backend/syncademic/models/capacitacion.py
@@ -5,7 +5,7 @@ from .periodo import Periodo
 
 class Capacitacion(models.Model):
     id_capacitacion = models.AutoField(primary_key=True)
-    docente = models.ForeignKey(Docente, on_delete=models.SET_NULL, default=1)
+    docente = models.ForeignKey(Docente, on_delete=models.SET_NULL, default=1, null=True)
     nombre_capacitacion = models.CharField(max_length=100)
     area = models.CharField(max_length=100)
     periodo = models.ForeignKey(Periodo, on_delete=models.SET_DEFAULT, default=6)
